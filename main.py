4f539645c373535cfecaea3085ec3287cb689fb1
alifmazli <alifmazli01@gmail.com>
2021-10-30 14:58:45 +0800

    added dropdown selection for audio playback

--- main.py
+++ main.py
@@ -9,18 +9,31 @@ def convert_to_wav(non_wav_file):
 def compute_audio(wav_file):
     pass
 
+# function to get filename from a list
 def get_file_names(files):
     file_names = []
     for i in range(len(files)):
         file_names.append(files[i].name)
     return file_names
 
+# function to display audio files based on dropdown selection
+def display_audio_playback(file_name, files):
+    for i in range(len(files)):
+        if file_name == files[i].name:
+            st.audio(files[i])
+
 st.header('Gnosis')
 
-uploaded_files = st.file_uploader("Upload an audio file (wav, mp3)", type=['wav','mp3'], accept_multiple_files=True)
-
-
-st.selectbox(
-    "Choose audio file to play",
-    get_file_names(uploaded_files)
+uploaded_files = st.file_uploader(
+    "Upload audio file(s)",
+    type=['wav','mp3'],
+    accept_multiple_files=True
 )
+
+dropdown_selection = st.selectbox(
+    "Choose audio file to play",
+    get_file_names(uploaded_files), 
+    1
+)
+
+display_audio_playback(dropdown_selection, uploaded_files)
