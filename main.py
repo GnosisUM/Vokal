a78f5b9f945026523ca7709c0ded5da50eed1321
Syaamil Faiq <syaamil1136@gmail.com>
2021-10-31 00:09:58 +0800

    remove dupes

--- main.py
+++ main.py
@@ -50,24 +50,3 @@ with col2:
     # Displays only the audio player when number of files == 1
     if len(uploaded_files) == 1:
         st.audio(uploaded_files[0])    
-
-# File uploader
-# uploaded_files = st.file_uploader(
-#     "Upload audio file(s)",
-#     type=['wav','mp3'],
-#     accept_multiple_files=True
-# )
-
-# Displays dropdown menu if number of files > 1
-# if len(uploaded_files) > 1:
-#     dropdown_selection = st.selectbox(
-#         "Choose audio file to play",
-#         get_file_names(uploaded_files),
-#         1
-#     )
-
-#     display_audio_playback(dropdown_selection, uploaded_files)
-
-# # Displays only the audio player when number of files == 1
-# if len(uploaded_files) == 1:
-#     st.audio(uploaded_files[0])
